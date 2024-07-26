8eacbdf74267dd53f7b6587a866da04b7950313c
real og <70781219+frick8@users.noreply.github.com>
2024-07-26 00:32:25 -0400

    aaaaaa

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,16 @@ document.addEventListener("DOMContentLoaded", function() {
 
     toggleButton.addEventListener("click", function() {
         sidebar.classList.toggle("collapsed");
+        document.getElementById("content").classList.toggle("collapsed");
     });
 
     window.addEventListener("resize", function() {
         if (window.innerWidth <= 768) {
             sidebar.classList.add("collapsed");
+            document.getElementById("content").classList.add("collapsed");
         } else {
             sidebar.classList.remove("collapsed");
+            document.getElementById("content").classList.remove("collapsed");
         }
     });
 });
