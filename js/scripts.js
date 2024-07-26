f58ddf3fce2a7751877353e938b676a8a8c6b89f
real og <70781219+frick8@users.noreply.github.com>
2024-07-26 01:36:42 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
-    document.body.classList.add("fade-in");
-
     let lastScrollTop = 0;
     const header = document.querySelector("header");
 
     window.addEventListener("scroll", function() {
         let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-        if (scrollTop > lastScrollTop && scrollTop > header.offsetHeight) {
+        if (scrollTop > lastScrollTop) {
             header.classList.add("hidden");
-        } else if (scrollTop < lastScrollTop) {
+        } else {
             header.classList.remove("hidden");
         }
         lastScrollTop = scrollTop;
