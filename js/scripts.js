c98a325d711782ebb0ae7e0de28204ce732dc3ad
real og <70781219+frick8@users.noreply.github.com>
2024-07-26 01:56:38 -0400

    hh

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
     let lastScrollTop = 0;
+    let scrollTimeout;
     const header = document.querySelector("header");
 
     window.addEventListener("scroll", function() {
+        clearTimeout(scrollTimeout);
+
         let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-        if (scrollTop > lastScrollTop) {
-            header.classList.add("hidden");
-        } else {
-            header.classList.remove("hidden");
-        }
-        lastScrollTop = scrollTop;
+        
+        // Debounce the scroll event handling
+        scrollTimeout = setTimeout(function() {
+            if (scrollTop > lastScrollTop && scrollTop > header.offsetHeight) {
+                header.classList.add("hidden");
+            } else if (scrollTop < lastScrollTop) {
+                header.classList.remove("hidden");
+            }
+            lastScrollTop = scrollTop;
+        }, 100);
     });
 });
